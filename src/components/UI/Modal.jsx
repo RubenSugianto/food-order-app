3cdba4f346539d7de295f92d43f38ae2e38465d5
RubenSugianto <ruben.sugianto@gmail.com>
2025-03-24 22:09:55 +0700

    Opening the Cart in the Modal via a New Context

--- src/components/UI/Modal.jsx
+++ src/components/UI/Modal.jsx
@@ -1,15 +1,18 @@
+import { useRef } from "react";
 import { useEffect } from "react";
 import { createPortal } from "react-dom";
-
-
 
 export default function Modal({ children, open, className= '' }) {
     const dialog = useRef();
 
     useEffect(() => {
+        const modal = dialog.current;
+
         if (open) {
-            dialog.current.showModal();
+            modal.showModal();
         } 
+
+        return () => modal.close();
     }, [open]);
 
     return createPortal(
