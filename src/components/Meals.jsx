4d0640652abfc718cb6683784b7b3aa3f9eedb03
RubenSugianto <ruben.sugianto@gmail.com>
2025-03-26 00:28:54 +0700

    Adding a Custom HTTP Hook & Avoiding Common Errors

--- src/components/Meals.jsx
+++ src/components/Meals.jsx
@@ -1,23 +1,26 @@
-import { useState, useEffect } from 'react';
 import MealItem from './MealItem.jsx';
+import useHttp from '../hooks/useHttp.js';
+
+const requestConfig = {};
 
 export default function Meals() {
-    const [loadedMeals, setLoadedMeals] = useState([]);
+    // pake param ketiiga [] sebagai initialData biar ga error di loadedMeals nya.
+    // kalo req selesai dia akan buat lagi param kedua baru terus" an jd infinite loop
+    // makanya harus dibuat jad global diset dulu biar kita selalu pake object / memory yang sama disini
+    // untuk param kedua..
+    const {
+        data: loadedMeals,
+        isLoading,
+        error,
+    } = useHttp('http://localhost:3000/meals', requestConfig, []);
 
-    useEffect(() => {
-        async function fetchMeals() {
-            const response = await fetch('http://localhost:3000/meals');
-    
-            if(!response.ok) {
-                //...
-            }
-    
-            const meals = await response.json();
-            setLoadedMeals(meals);
-        }
+    if (isLoading) {
+        return <p>Fetching meals...</p>;
+    }
 
-        fetchMeals();
-    }, []);
+    // if (!data) {
+    //     return <p>No meals found.</p>;
+    // }
 
     return (
         <ul id="meals">
